873db8d294f9b44b675c3aa52761114133e709ec
Felipe <felipe.cristo@yahoo.com.br>
2022-06-13 20:47:18 -0300

    up

---
+++ public/js/script.js
@@ -0,0 +1,11 @@
+const closeMessage = document.querySelector('#close-message');
+const mensagem = document.querySelector('.message');
+
+closeMessage.addEventListener('click',function(){
+    mensagem.getElementsByClassName.display = "none";
+});
+
+setTimeout(()=>{
+    mensagem.getElementsByClassName.display = 'none';
+
+},5000);
